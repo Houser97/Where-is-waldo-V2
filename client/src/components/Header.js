b2d11ba490691d75c650f28076976106df079cc5
Houser97 <arturo.riverar97@gmail.com>
2022-12-25 21:49:26 -0600

    Set up Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,44 @@
+import '../styles/header.css';
+import logo from '../images/Adventure-time-logo.png';
+import BMO from '../images/BMO.jpeg';
+import Marceline from '../images/marceline.png'
+import Trunks from '../images/Trunks.jpeg'
+import Timer from './timer';
+import { useRef } from 'react';
+
+const Header = () => {
+    const BMOelement = useRef(null);
+    const MarcelineElement = useRef(null);
+    const Tree = useRef(null);
+
+    return(
+        <header>
+            <div className='characters-DIV'>
+                <div className='where-is'>Where is...?</div>
+                <div className='characters'>
+                    <div className='photo-name'>
+                        <img ref={BMOelement} src={BMO} alt='BMO' className='first-photo character'></img>
+                        <div className='name'>BMO</div>
+                    </div>
+
+                    <div className='photo-name'>
+                        <img ref={MarcelineElement} src={Marceline} alt='Marceline' className='first-photo character'></img>
+                        <div className='name'>Marceline</div>
+                    </div>
+
+                    <div className='photo-name'>
+                        <img ref={Tree} src={Trunks} alt='Tree Trunks' className='first-photo character'></img>
+                        <div className='name'>Tree Trunks</div>
+                    </div>
+                </div>
+            </div>
+            <img src={logo} alt = "logo" className='logo'></img>
+            <div className='timer'>
+                <div>Timer</div>
+                <Timer />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
