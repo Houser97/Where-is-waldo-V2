c1d9225ea22e5d7a45ad859d0708c125b817a978
Houser97 <arturo.riverar97@gmail.com>
2022-12-26 15:22:26 -0600

    Wrap content to apply padding and grid

--- client/src/components/Characters.js
+++ client/src/components/Characters.js
@@ -4,17 +4,26 @@ import { CHARACTERS } from '../assets'
 
 const Characters = () => {
   return (
-    <div className='characters'>
-    {
-        CHARACTERS.map((character, i) => {
-            return(
-                <div key={i} className='photo-name'>
-                    <img src={character.image} alt={character.name} className='first-photo character'></img>
-                    <div className='name'>{character.name}</div>
+    <div className='characters-background'>
+        <div className='characters'>
+            <div className='title'>Where are ...?</div>
+            <div className='content'>
+                <div className='description'>Find the following three characters in the shortest time possible to appear in the list of the fastest.</div>
+                <div className='characters-grid'>
+                    {
+                        CHARACTERS.map((character, i) => {
+                            return(
+                                <div key={i} className='photo-name'>
+                                    <img src={character.image} alt={character.name} className='first-photo character'></img>
+                                    <div className='name'>{character.name}</div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-            )
-        })
-    }
+                <div className='start-game'>Start</div>
+            </div>
+        </div>
     </div>
   )
 }
