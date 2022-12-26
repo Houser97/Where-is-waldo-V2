287d18ac20b241844f9f815a2106f8ba2e30bf47
Houser97 <arturo.riverar97@gmail.com>
2022-12-26 16:43:31 -0600

    Import character images and set them in object to be exported

---
+++ client/src/assets/index.js
@@ -0,0 +1,18 @@
+import Kratos from './Images/Kratos.png';
+import Ash from './Images/Ash.png';
+import Tom from './Images/Tom.png';
+
+export const CHARACTERS = [
+    {
+        image: Kratos,
+        name: 'Kratos'
+    }, 
+    {
+        image: Ash,
+        name: 'Ash'
+    }, 
+    {
+        image: Tom,
+        name: 'Tom'
+    }
+];
