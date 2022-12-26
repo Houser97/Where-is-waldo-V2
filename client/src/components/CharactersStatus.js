87bed81c7af23a9b1437b76e103321330d69dab3
Houser97 <arturo.riverar97@gmail.com>
2022-12-26 16:34:04 -0600

    Set up component

---
+++ client/src/components/CharactersStatus.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/CharactersStatus.css'
+import { CHARACTERS } from '../assets'
+
+const CharactersStatus = ({toggleSvg}) => {
+  return (
+    <div className={`characters-status ${toggleSvg ? 'open-status':''}`}>
+        {
+            CHARACTERS.map((character, i) => {
+                return(
+                    <div className='character-status'>
+                        <img className='character-status-image' src={character.image}></img>
+                        <div className='character-status-name'>{character.name}</div>
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default CharactersStatus
