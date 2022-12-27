e03a70166131ecbe1822726fd86bd671df7fe189
Houser97 <arturo.riverar97@gmail.com>
2022-12-27 17:31:07 -0600

    Import mongoose and dotenv

--- server/app.js
+++ server/app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
