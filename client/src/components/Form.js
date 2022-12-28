38ae3f4d43d14e89f711263608a627b97b0f3f1c
Houser97 <arturo.riverar97@gmail.com>
2022-12-28 10:53:30 -0600

    Set up Form component

---
+++ client/src/components/Form.js
@@ -0,0 +1,27 @@
+import '../styles/Form.css';
+
+const Form = ({getUserName, gameOver}) => {
+
+    const openLadderboard = () => {
+        const ladder =document.querySelector(".ladderboard-section");
+        ladder.style.display = "flex";
+    }
+
+    return(
+        <div className={`popup-form ${gameOver}`}>
+            <div className='win'>You win!</div>
+            <form className='form' onSubmit={getUserName}>
+                <div className='form-title'>Enter a name to save your time</div>
+                <div className='input-section'>
+                    <label htmlFor='name'>Name</label>
+                    <input id='name' name='name' required></input>
+                </div>
+                <div className='button-section'>
+                    <button className='submit' onClick={openLadderboard}>Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
