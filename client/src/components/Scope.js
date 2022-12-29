7fbb8a8f9e8939fabfcfef2a46726216c9894af8
Houser97 <arturo.riverar97@gmail.com>
2022-12-28 22:57:23 -0600

    Set functions to handle character selection and removal from list

--- client/src/components/Scope.js
+++ client/src/components/Scope.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Scope.css'
 import { CHARACTERS } from '../assets';
 
-const Scope = () => {
+const Scope = ({scopeRef}) => {
+
+    const removeCharacterFromList = (liElement) => {
+        liElement.target.style.display = 'none'
+    }
+
+    const getValueLi = (e) => {
+        const CHARACTER = e.target.textContent;
+        fetch(`http://localhost:5000/api/get_coordinates/${CHARACTER}`)
+        .then(response => response.json())
+        .then(data => {
+          if(/*checkIfSelected(coordsUser.x, coordsUser.y, data.x, data.y, CHARACTER)*/true){
+            /*
+            removeCharacterFromList(e)
+            setToggle("show");
+            setMessage(`You have found ${CHARACTER}!`);
+            setNumberOfCharacters(number => number - 1);*/
+          } else {
+            /*setToggle("show-incorrect");
+            setMessage(CHARACTER);
+            setMessage("Keep trying")*/
+          }
+          const magicDiv = scopeRef.current;
+          magicDiv.style.display = "none";
+        })
+      }
+
   return (
-    <div className='magic-div' ref={square}>
+    <div className='magic-div' ref={scopeRef}>
         <div className='x-design-1 x-design'></div>
         <div className='x-design-2 x-design'></div>
         <div className='container-list'>
@@ -15,7 +41,6 @@ const Scope = () => {
                     <li key={`li-${i}`} 
                     data-id = {i}
                     className={`li-element ${i < CHARACTERS.length -1 ? '':'last-li-element'}`} 
-                    ref={element => charactersArray.current[i] = element} 
                     onClick = {getValueLi}>{character.name}</li>
                     )
                 })
