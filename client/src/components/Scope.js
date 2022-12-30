bfc792019bf710cecb90bc6216ac1d0092c802d0
Houser97 <arturo.riverar97@gmail.com>
2022-12-29 20:24:36 -0600

    Remove unused logic from functions

--- client/src/components/Scope.js
+++ client/src/components/Scope.js
@@ -3,30 +3,39 @@ import '../styles/Scope.css'
 import { CHARACTERS } from '../assets';
 import { gameContext } from '../App';
 
-const Scope = ({scopeRef}) => {
-
-    const setToggleMessage = useContext(gameContext).setToggleMessage;
-    const setMessage = useContext(gameContext).setMessage;
+const Scope = ({scopeRef, coordsUser}) => {
 
     const removeCharacterFromList = (liElement) => {
         liElement.target.style.display = 'none'
     }
 
-    const getValueLi = (e) => {
+    const checkIfSelected = (solutionX, solutionY) => {
+      let componentX = Math.pow(Math.abs(coordsUser.x-solutionX),2);
+      let componentY = Math.pow(Math.abs(coordsUser.y-solutionY),2)
+      let distance = Math.round(Math.sqrt(componentX+componentY));
+
+      if(distance < 5){
+        return true
+      } 
+      return false  
+    }
+
+    const checkHit = (e) => {
         const CHARACTER = e.target.textContent;
         fetch(`http://localhost:5000/api/get_coordinates/${CHARACTER}`)
         .then(response => response.json())
         .then(data => {
-          if(/*checkIfSelected(coordsUser.x, coordsUser.y, data.x, data.y, CHARACTER)*/true){
+          if(checkIfSelected(data.x, data.y)){
             removeCharacterFromList(e)
-            setToggleMessage(true);
+            console.log(CHARACTER);
+            /*setToggleMessage(true);
             setMessage(`You have found ${CHARACTER}!`);
-            /*
+            
             removeCharacterFromList(e)
             setToggle("show");
             setMessage(`You have found ${CHARACTER}!`);
             setNumberOfCharacters(number => number - 1);*/
-          } else {
+          } else {console.log('Keep trying');
             /*setToggle("show-incorrect");
             setMessage(CHARACTER);
             setMessage("Keep trying")*/
@@ -43,14 +52,14 @@ const Scope = ({scopeRef}) => {
         <div className='container-list'>
             <ul className='list-characters'>
                 {
-                CHARACTERS.map((character, i) => {
-                    return(
-                    <li key={`li-${i}`} 
-                    data-id = {i}
-                    className={`li-element ${i < CHARACTERS.length -1 ? '':'last-li-element'}`} 
-                    onClick = {getValueLi}>{character.name}</li>
-                    )
-                })
+                  CHARACTERS.map((character, i) => {
+                      return(
+                      <li key={`li-${i}`} 
+                      data-id = {i}
+                      className={`li-element ${i < CHARACTERS.length -1 ? '':'last-li-element'}`} 
+                      onClick = {checkHit}>{character.name}</li>
+                      )
+                  })
                 }
             </ul>
         </div>
