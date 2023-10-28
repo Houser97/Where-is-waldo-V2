74e8dd624cb4b8cf4f86e6c2b176f772d1f9b0d0
Arturo Rivera <arturo.riverar97@gmail.com>
2023-10-27 21:45:23 -0600

    Transform Characters array into an object

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,17 +2,17 @@ import Kratos from './Images/Kratos.png';
 import Ash from './Images/Ash.png';
 import Tom from './Images/Tom.png';
 
-export const CHARACTERS = [
-    {
+export const CHARACTERS = {
+    'kratos':{
         image: Kratos,
         name: 'Kratos'
     }, 
-    {
+    'Ash':{
         image: Ash,
         name: 'Ash'
     }, 
-    {
+    'Tom':{
         image: Tom,
         name: 'Tom'
     }
-];
+};
