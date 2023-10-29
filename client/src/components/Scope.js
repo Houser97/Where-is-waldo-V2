ac8a3284aa408ea011fed961715350927d1e3a02
Arturo Rivera <arturo.riverar97@gmail.com>
2023-10-29 16:17:53 -0600

    Remove unnecessary elements in Scope component

--- client/src/components/Scope.js
+++ client/src/components/Scope.js
@@ -1,52 +1,11 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import '../styles/Scope.css'
-import { gameContext } from '../App';
-import CharactersList from './CharactersList';
 
-const Scope = ({scopeRef, coordsUser}) => {
-
-  const setCharacterHit = useContext(gameContext).setCharacterHit
-  const setToggleMessage = useContext(gameContext).setToggleMessage
-
-
-    const removeCharacterFromList = (liElement) => {
-        liElement.target.style.display = 'none'
-    }
-
-    const checkIfSelected = (solutionX, solutionY) => {
-      let componentX = Math.pow(Math.abs(coordsUser.x-solutionX),2);
-      let componentY = Math.pow(Math.abs(coordsUser.y-solutionY),2)
-      let distance = Math.round(Math.sqrt(componentX+componentY));
-
-      if(distance < 5){
-        return true
-      } 
-      return false  
-    }
-
-    const getCharacter = (e) => {
-        const CHARACTER = e.target.textContent;
-        fetch(`/api/get_coordinates/${CHARACTER}`)
-        .then(response => response.json())
-        .then(data => {
-          if(checkIfSelected(data.x, data.y)){
-            removeCharacterFromList(e);
-            setCharacterHit(CHARACTER);
-          } else {
-            setCharacterHit(false); // Se limpia para que Message renderice 'Keep Trying'.
-            setToggleMessage(true); // Renderiza mensaje con 'Keep Trying'
-          }
-          const magicDiv = scopeRef.current
-          magicDiv.style.display = 'none'
-        })
-
-      }
-
+const Scope = ({scopeRef}) => {
   return (
     <div className='magic-div' ref={scopeRef}>
         <div className='x-design-1 x-design'></div>
         <div className='x-design-2 x-design'></div>
-        <CharactersList getCharacter={getCharacter} />
     </div>
   )
 }
