2f577135b5e3a23a921490a21c976889af2efa25
Arturo Rivera <arturo.riverar97@gmail.com>
2023-10-29 14:14:43 -0600

    Move CharacterList logic to respective component

--- client/src/components/Scope.js
+++ client/src/components/Scope.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import '../styles/Scope.css'
-import { CHARACTERS } from '../assets';
 import { gameContext } from '../App';
+import CharactersList from './CharactersList';
 
 const Scope = ({scopeRef, coordsUser}) => {
 
@@ -46,20 +46,7 @@ const Scope = ({scopeRef, coordsUser}) => {
     <div className='magic-div' ref={scopeRef}>
         <div className='x-design-1 x-design'></div>
         <div className='x-design-2 x-design'></div>
-        <div className='container-list'>
-            <ul className='list-characters'>
-                {
-                  Object.keys(CHARACTERS).map((character, i) => {
-                      return(
-                      <li key={`li-${i}`} 
-                      data-id = {i}
-                      className={`li-element ${i < Object.keys(CHARACTERS).length -1 ? '':'last-li-element'}`} 
-                      onClick = {getCharacter}><img src={CHARACTERS[character].image} className='img__character-item'></img><span>{CHARACTERS[character].name}</span></li>
-                      )
-                  })
-                }
-            </ul>
-        </div>
+        <CharactersList getCharacter={getCharacter} />
     </div>
   )
 }
