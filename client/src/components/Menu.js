1b874b1fcb0d56177cfec9ba4dd59fa512dfc9aa
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-08 21:41:21 -0600

    Create Menu component

---
+++ client/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Menu.css'
+import Gallery from './Menu/Gallery'
+
+const Menu = () => {
+  return (
+    <div className='menu__container'>
+        <Gallery />
+    </div>
+  )
+}
+
+export default Menu
