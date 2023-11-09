bb9a7e40326292431d5adbdd23881849a7e81f53
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-08 21:28:15 -0600

    Adjust values of data-text attribute

--- client/src/components/Menu/Gallery.js
+++ client/src/components/Menu/Gallery.js
@@ -4,9 +4,9 @@ import '../../styles/Menu/Gallery.css'
 const Gallery= () => {
   return (
     <div className="gallery__container">
-        <div className="gallery one" data-text="Board 1"></div>
-        <div className="gallery two" data-text="Board 2"></div>
-        <div className="gallery three" data-text="Board 3"></div>
+        <div className="gallery one" data-text="Cyberpunk City"></div>
+        <div className="gallery two" data-text="Robot City"></div>
+        <div className="gallery three" data-text="Universe 113"></div>
     </div>
   )
 }
