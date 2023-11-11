dbc98f307b3fe3ec277a947bf03201fcfd9f1256
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-10 18:18:00 -0600

    Set router object to switch between Menu and Board

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Menu from './components/Menu';
+import Board from './components/Board';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />
+  },
+  {
+    path: '/:idGame',
+    element: <App />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
