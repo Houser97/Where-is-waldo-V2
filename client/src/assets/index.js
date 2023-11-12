cb20894d2ba7b3b41714d4fde0e88f5f741f6941
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-11 23:08:19 -0600

    Create object to store images for different games

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,6 +1,9 @@
 import Kratos from './Images/Kratos.png';
 import Ash from './Images/Ash.png';
 import Tom from './Images/Tom.png';
+import Cyberpunk from './Images/BackgroundWaldo.jpg'
+import RobotCity from './Images/RobotCity.jpg'
+import Universe113 from './Images/Universe113.jpg'
 
 export const CHARACTERS = {
     'Kratos':{
@@ -13,3 +16,9 @@ export const CHARACTERS = {
         image: Tom,
     }
 };
+
+export const BOARDS = {
+    'Cyberpunk-City': Cyberpunk,
+    'Robot-City':RobotCity,
+    'Universe-113':Universe113
+}
