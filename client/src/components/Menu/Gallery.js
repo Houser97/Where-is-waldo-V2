d02ab9b472850da773068beb693f0777aa604500
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-11 23:12:45 -0600

    Import Link component to change route

--- client/src/components/Menu/Gallery.js
+++ client/src/components/Menu/Gallery.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../../styles/Menu/Gallery.css'
+import { Link } from "react-router-dom";
 
 const Gallery= () => {
   return (
+  
     <div className="gallery__container">
-        <div className="gallery one" data-text="Cyberpunk City"></div>
-        <div className="gallery two" data-text="Robot City"></div>
-        <div className="gallery three" data-text="Universe 113"></div>
+        <Link to='Cyberpunk-City' className="gallery one" data-text="Cyberpunk City"></Link>
+        <Link to='Robot-City' className="gallery two" data-text="Robot City"></Link>
+        <Link to='Universe-113' className="gallery three" data-text="Universe 113"></Link>
     </div>
   )
 }
