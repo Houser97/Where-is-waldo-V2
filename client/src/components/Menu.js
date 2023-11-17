5270cd153edb6c63e4d51743a7c9ca0bcba01809
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-16 21:37:54 -0600

    Import HeaderMenu component

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/Menu.css'
 import Gallery from './Menu/Gallery'
+import HeaderMenu from './Menu/HeaderMenu'
 
 const Menu = () => {
   return (
     <div className='menu__container'>
-        <Gallery />
+      <HeaderMenu>Where is...?</HeaderMenu>
+      <Gallery />
     </div>
   )
 }
