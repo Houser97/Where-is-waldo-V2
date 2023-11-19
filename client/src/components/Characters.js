72be386cd7232c422c47048f326b5f47e0ec8fe0
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-18 23:28:12 -0600

    Update CHARACTERS reading to display characters based on board

--- client/src/components/Characters.js
+++ client/src/components/Characters.js
@@ -2,11 +2,16 @@ import React, { useContext } from 'react'
 import '../styles/Characters.css'
 import { CHARACTERS } from '../assets'
 import { gameContext } from '../App'
+import { useParams } from 'react-router-dom'
 
 const Characters = () => {
 
     const startGame = useContext(gameContext).startGame;
     const setStartGame = useContext(gameContext).setStartGame;
+
+    const { idGame } = useParams();
+
+    const CHARACTERS_BOARD = CHARACTERS[idGame]
 
   return (
     <div className={`characters-background ${startGame ? 'hide':''}`}>
@@ -16,11 +21,11 @@ const Characters = () => {
                 <div className='description'>Find the following three characters in the shortest time possible to appear in the list of the fastest.</div>
                 <div className='characters-grid'>
                     {
-                        Object.keys(CHARACTERS).map((character, i) => {
+                        Object.keys(CHARACTERS_BOARD).map((character, i) => {
                             return(
                                 <div key={i} className='photo-name'>
-                                    <img src={CHARACTERS[character].image} alt={CHARACTERS[character].name} className='first-photo character'></img>
-                                    <div className='name'>{CHARACTERS[character].name}</div>
+                                    <img src={CHARACTERS_BOARD[character].image} alt={CHARACTERS_BOARD[character].name} className='first-photo character'></img>
+                                    <div className='name'>{character}</div>
                                 </div>
                             )
                         })
