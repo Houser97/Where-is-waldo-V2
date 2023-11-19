864727a6830a74f1a6049401a88e028e7507111d
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-19 13:28:24 -0600

    Add characters for the Universe-113 board

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -11,14 +11,25 @@ export const CHARACTERS = {
         }
     },
     'Robot-City': {
-        'Predator':{
-            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1700413059/imgbin_predator-png_1_1_p05z2n.png',
-        }, 
         'Mojo Jojo':{
             image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1699808617/Where%20is%20Waldo/pngwing.com_1_jq14p6.png',
         }, 
+        'Predator':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1700413059/imgbin_predator-png_1_1_p05z2n.png',
+        }, 
         'Wazowski':{
             image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1700370447/pngwing.com_2_1_wkru11.png',
+        }
+    },
+    'Universe-113': {
+        'Courage':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1700415826/Where%20is%20Waldo/pngwing.com_3_jpakim.png',
+        }, 
+        'Saitama':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1700421182/imgbin_one-punch-man-saitama-anime-manga-character-png_get52e.png',
+        }, 
+        'Groot':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1700420606/Where%20is%20Waldo/groot_qwfqmt.png',
         }
     },
 };
