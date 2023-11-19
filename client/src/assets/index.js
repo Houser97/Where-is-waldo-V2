01cd9f277fa36e46b1e075e699b1962386967982
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-18 23:23:32 -0600

    Add Characters image for Cyberpunk and Robot City Boards

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,13 +1,26 @@
 export const CHARACTERS = {
-    'Kratos':{
-        image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1699807424/Where%20is%20Waldo/Kratos_fpssgr.png',
-    }, 
-    'Ash':{
-        image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1699807413/Where%20is%20Waldo/Ash_mryet9.png',
-    }, 
-    'Tom':{
-        image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1699807430/Where%20is%20Waldo/Tom_aakdwn.png',
-    }
+    'Cyberpunk-City': {
+        'Kratos':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1699807424/Where%20is%20Waldo/Kratos_fpssgr.png',
+        }, 
+        'Ash':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1699807413/Where%20is%20Waldo/Ash_mryet9.png',
+        }, 
+        'Tom':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1699807430/Where%20is%20Waldo/Tom_aakdwn.png',
+        }
+    },
+    'Robot-City': {
+        'Depredator':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1700370295/depredator_1_ybptcj.png',
+        }, 
+        'Mojo Jojo':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1699808617/Where%20is%20Waldo/pngwing.com_1_jq14p6.png',
+        }, 
+        'Wazowski':{
+            image: 'https://res.cloudinary.com/dluwqcce9/image/upload/v1700370447/pngwing.com_2_1_wkru11.png',
+        }
+    },
 };
 
 export const BOARDS = {
