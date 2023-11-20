63dbb5c3a64a663c979851f806122473f7bb0101
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-20 14:59:09 -0600

    Set img element and useImageIsLoaded hook
    The img element is hidden from the user since its function is to
    change the value of the state indicating if the image has been loaded.
    This is done to display the Loader component only if the heaviest
    image has loaded.

--- client/src/components/Menu/Gallery.js
+++ client/src/components/Menu/Gallery.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import '../../styles/Menu/Gallery.css'
 import { Link } from "react-router-dom";
+import { useImageIsLoaded } from '../hooks/useImageIsLoaded';
+import Loader from '../Loader';
 
 const Gallery= () => {
+
+  const [ isImageLoaded, handleImageLoad ] = useImageIsLoaded();
+ 
   return (
-  
-    <div className="gallery__container">
+    <div className={`${isImageLoaded ? 'gallery__container' : 'loader__container'} `}>
+      {isImageLoaded 
+      ? <>
         <Link to='Cyberpunk-City' className="gallery one" data-text="Cyberpunk City"></Link>
         <Link to='Robot-City' className="gallery two" data-text="Robot City"></Link>
         <Link to='Universe-113' className="gallery three" data-text="Universe 113"></Link>
-    </div>
+      </> 
+      : <Loader />}
+        <img className='gallery__image-loader' alt='board' src='https://res.cloudinary.com/dluwqcce9/image/upload/v1700413484/CyberpunkCity_dzadne.jpg' onLoad={handleImageLoad}></img>
+    </div>  
   )
 }
 
