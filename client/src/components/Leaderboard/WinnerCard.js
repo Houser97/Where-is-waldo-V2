298fab53833724489ae3a329b0c728ce4361197c
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-26 20:17:48 -0600

    Add classes according to the user's position

--- client/src/components/Leaderboard/WinnerCard.js
+++ client/src/components/Leaderboard/WinnerCard.js
@@ -2,10 +2,17 @@ import React from 'react'
 import '../../styles/Leaderboard/WinnerCard.css'
 
 const WinnerCard = ({winnerPosition, username, time, profileImg}) => {
+  
+  const winnerClass = winnerPosition === 1 
+  ? 'first_place' 
+  : winnerPosition === 2 
+  ? 'second_place' 
+  : 'third_place'
+  
   return (
-    <div className='winnerCard__container'>
+    <div className={`winnerCard__container ${winnerClass}`}>
         <div className='image__container'>
-            <img alt='winner' src={profileImg}></img>
+            <img alt='winner' src={profileImg} ></img>
             <span className='winner__place'>{winnerPosition}</span>
         </div>
         <div className='winner__data'>
