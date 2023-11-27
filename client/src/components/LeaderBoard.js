b47c6312948df11bad53af12d03aaaf0eda94831
Arturo Rivera <arturo.riverar97@gmail.com>
2023-11-26 20:21:14 -0600

    Wrap WinnerCard component in a div
    The WinnerCard component should be in a grid container to control the
    components location according to the user's position.

--- client/src/components/LeaderBoard.js
+++ client/src/components/LeaderBoard.js
@@ -5,7 +5,11 @@ import WinnerCard from './Leaderboard/WinnerCard'
 const LeaderBoard = () => {
   return (
     <div className='leaderboard__container'>
-      <WinnerCard />
+      <div className='winners__container'>
+        <WinnerCard profileImg={'https://res.cloudinary.com/dluwqcce9/image/upload/v1697390686/InTouch/irlci3ocrmxri0dlxdz0.jpg'} username={'House'} time={'02:03'} winnerPosition={1}/>
+        <WinnerCard profileImg={'https://res.cloudinary.com/dluwqcce9/image/upload/v1697735636/InTouch/lrlmwnzacirgh3sdblyo.jpg'} username={'Bethoveen'} time={'03:12'} winnerPosition={2}/>
+        <WinnerCard profileImg={'https://res.cloudinary.com/dluwqcce9/image/upload/v1697392849/InTouch/lzxr2waxbhvewptjbegy.jpg'} username={'Lucky'} time={'04:43'} winnerPosition={3}/>
+      </div>
     </div>
   )
 }
