c859d912c43eeb6c88daa35b51cd01cc142888e1
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-02 23:04:47 -0600

    Add game and image fields in user model

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: {type: String, required: true},
     time: {type: Number, required: true},
+    game: {type: String, required: true},
+    image: {type: String, default: "https://res.cloudinary.com/dluwqcce9/image/upload/v1694961227/InTouch/qqaarw68ruwwluvcphkh.jpg"},
 })
 
 module.exports = mongoose.model('User', UserSchema);
