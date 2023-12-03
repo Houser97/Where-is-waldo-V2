83dea17af9420ebb4b29a30f42b487c81ecfffce
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-03 12:22:59 -0600

    Wrap username, image and position in div

--- client/src/components/Leaderboard/PlayerCard.js
+++ client/src/components/Leaderboard/PlayerCard.js
@@ -4,10 +4,12 @@ import '../../styles/Leaderboard/PlayerCard.css'
 const PlayerCard = ({profileImg, username, time, position}) => {
   return (
     <div className='playercard__container'>
-        <span className='player__position'>{position}</span>
-        <img className='' src={profileImg} alt='player_image'></img>
-        <span className='player__username'>{username}</span>
-        <span className='player__time'>{time}</span>
+        <div className='player__data'>
+            <span className='player__position'>{position}.</span>
+            <img className='' src={profileImg} alt='player_image'></img>
+            <span className='player__username'>{username}</span>
+        </div>
+        <div className='player__time'>{time}</div>
     </div>
   )
 }
