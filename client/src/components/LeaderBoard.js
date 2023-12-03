ab6cf06548f437806153c8151572e7fd65d434dc
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-03 12:24:44 -0600

    Set players container div and import PlayerCard

--- client/src/components/LeaderBoard.js
+++ client/src/components/LeaderBoard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/LeaderBoard.css'
+import PlayerCard from './Leaderboard/PlayerCard'
 import WinnerCard from './Leaderboard/WinnerCard'
 
 const LeaderBoard = () => {
@@ -9,6 +10,9 @@ const LeaderBoard = () => {
         <WinnerCard profileImg={'https://res.cloudinary.com/dluwqcce9/image/upload/v1697390686/InTouch/irlci3ocrmxri0dlxdz0.jpg'} username={'House'} time={'02:03'} winnerPosition={1}/>
         <WinnerCard profileImg={'https://res.cloudinary.com/dluwqcce9/image/upload/v1697735636/InTouch/lrlmwnzacirgh3sdblyo.jpg'} username={'Bethoveen'} time={'03:12'} winnerPosition={2}/>
         <WinnerCard profileImg={'https://res.cloudinary.com/dluwqcce9/image/upload/v1697392849/InTouch/lzxr2waxbhvewptjbegy.jpg'} username={'Lucky'} time={'04:43'} winnerPosition={3}/>
+      </div>
+      <div className='players__container'>
+
       </div>
     </div>
   )
