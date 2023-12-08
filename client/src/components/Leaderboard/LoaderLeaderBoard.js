02fee2c4b2019715e3a97d805e4dc5d4a4e0a5fc
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-08 11:05:10 -0600

    Set Loader for LeaderBoard component

---
+++ client/src/components/Leaderboard/LoaderLeaderBoard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../../styles/Leaderboard/LoaderLeaderBoard.css'
+
+const LoaderLeaderBoard = () => <span className="loader__leaderboard"></span>
+
+
+export default LoaderLeaderBoard
