b96bc4936a07fc8c53989a07dabd350be9362ea6
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-10 10:20:46 -0600

    Update set_scores controller to save profile image

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,10 +1,13 @@
 const User = require('../models/user');
 const Coordinate = require('../models/coordinate');
-const {body} = require('express-validator')
+const { body } = require('express-validator')
+const { cloudinary } = require('../services/cloudinary');
+
+const CLOUDINARY_PRESET = 'Waldo';
 
 exports.get_coordinates = (req, res) => {
-    Coordinate.findOne({character: req.params.character}, (err, character) => {
-        if(err) return res.json('Error');
+    Coordinate.findOne({ character: req.params.character }, (err, character) => {
+        if (err) return res.json('Error');
         return res.json(character)
     })
 }
@@ -12,16 +15,32 @@ exports.get_coordinates = (req, res) => {
 exports.set_score = [
     body('username').trim().escape(),
 
-    (req, res) => {
-        const user = new User({
-            username: req.body.username,
-            time: req.body.time,
-        }).save((err, user) => {
-            User.find().sort({time: 1}).exec((err, users) => {
-                if(err) return res.json('Error');
-                return res.json(users)
+    async (req, res) => {
+        const { image, username, time, game } = req.body;
+        if (!image.length) {
+            const user = await new User({
+                username,
+                time,
+                game,
+            }).save();
+
+            return res.json(true)
+        }
+        try {
+            const data = await cloudinary.uploader.upload(image, {
+                upload_preset: CLOUDINARY_PRESET
             })
-        })
+            const user = await new User({
+                username,
+                time,
+                game,
+                image: data.url
+            }).save();
+            res.json(user)
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ err: 'Something went wrong' })
+        }
     }
 ]
 
@@ -29,8 +48,8 @@ exports.get_scores = (req, res) => {
 
     const game = req.params.game
 
-    User.find({game: {$eq: game}}).sort({time: 1}).exec((err, users) => {
-        if(err) return res.json(false);
+    User.find({ game: { $eq: game } }).sort({ time: 1 }).exec((err, users) => {
+        if (err) return res.json(false);
         return res.json(users)
     })
 }
