9e70bfea097907797e424d369b7dff0a059c4240
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-16 14:31:45 -0600

    Add error handling, async/await, try/catch and HTTP response messages

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,55 +1,59 @@
 const User = require('../models/user');
 const Coordinate = require('../models/coordinate');
-const { body } = require('express-validator')
+const { body } = require('express-validator');
 const { cloudinary } = require('../services/cloudinary');
 
 const CLOUDINARY_PRESET = 'Waldo';
+const ERROR_MSG = 'Server error.';
 
-exports.get_coordinates = (req, res) => {
-    Coordinate.findOne({ character: req.params.character }, (err, character) => {
-        if (err) return res.json('Error');
-        return res.json(character)
-    })
-}
+exports.get_coordinates = async (req, res) => {
+    try {
+        const character = await Coordinate.findOne({ character: req.params.character });
+        if (!character) {
+            return res.status(404).json({ err: 'Coordinate not found' });
+        }
+        return res.status(200).json(character);
+    } catch (err) {
+        return res.status(500).json({ err: ERROR_MSG });
+    }
+};
 
 exports.set_score = [
     body('username').trim().escape(),
 
     async (req, res) => {
         const { image, username, time, game } = req.body;
-        if (!image.length) {
-            const user = await new User({
-                username,
-                time,
-                game,
-            }).save();
-
-            return res.json(true)
-        }
         try {
-            const data = await cloudinary.uploader.upload(image, {
-                upload_preset: CLOUDINARY_PRESET
-            })
-            const user = await new User({
-                username,
-                time,
-                game,
-                image: data.url
-            }).save();
-            res.json(user)
+            if (image && image.length) {
+                const data = await cloudinary.uploader.upload(image, {
+                    upload_preset: CLOUDINARY_PRESET,
+                });
+                await new User({
+                    username,
+                    time,
+                    game,
+                    image: data.url,
+                }).save();
+            } else {
+                await new User({
+                    username,
+                    time,
+                    game,
+                }).save();
+            }
+            return res.status(200).json('User saved');
         } catch (error) {
-            console.log(error);
-            res.status(500).json({ err: 'Something went wrong' })
+            return res.status(500).json({ err: error.message || ERROR_MSG });
         }
+    },
+];
+
+exports.get_scores = async (req, res) => {
+    try {
+        const game = req.params.game;
+        const users = await User.find({ game }).sort({ time: 1 }).exec();
+        return res.status(200).json(users);
+    } catch (err) {
+        return res.status(500).json({ err: ERROR_MSG });
     }
-]
-
-exports.get_scores = (req, res) => {
-
-    const game = req.params.game
-
-    User.find({ game: { $eq: game } }).sort({ time: 1 }).exec((err, users) => {
-        if (err) return res.json(false);
-        return res.json(users)
-    })
-}
+};
