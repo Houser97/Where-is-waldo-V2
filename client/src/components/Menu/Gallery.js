e241d654c5d70b19ea5fe3914278d70965292a43
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-17 21:52:36 -0600

    Remove unused documents and format code

--- client/src/components/Menu/Gallery.js
+++ client/src/components/Menu/Gallery.js
@@ -4,21 +4,21 @@ import { Link } from "react-router-dom";
 import { useImageIsLoaded } from '../hooks/useImageIsLoaded';
 import Loader from '../Loader';
 
-const Gallery= () => {
+const Gallery = () => {
 
-  const [ isImageLoaded, handleImageLoad ] = useImageIsLoaded();
- 
+  const [isImageLoaded, handleImageLoad] = useImageIsLoaded();
+
   return (
     <div className={`${isImageLoaded ? 'gallery__container' : 'loader__container'} `}>
-      {isImageLoaded 
-      ? <>
-        <Link to='Cyberpunk-City' className="gallery one" data-text="Cyberpunk City"></Link>
-        <Link to='Robot-City' className="gallery two" data-text="Robot City"></Link>
-        <Link to='Universe-113' className="gallery three" data-text="Universe 113"></Link>
-      </> 
-      : <Loader />}
-        <img className='gallery__image-loader' alt='board' src='https://res.cloudinary.com/dluwqcce9/image/upload/v1700413484/CyberpunkCity_dzadne.jpg' onLoad={handleImageLoad}></img>
-    </div>  
+      {isImageLoaded
+        ? <>
+          <Link to='Cyberpunk-City' className="gallery one" data-text="Cyberpunk City"></Link>
+          <Link to='Robot-City' className="gallery two" data-text="Robot City"></Link>
+          <Link to='Universe-113' className="gallery three" data-text="Universe 113"></Link>
+        </>
+        : <Loader />}
+      <img className='gallery__image-loader' alt='board' src='https://res.cloudinary.com/dluwqcce9/image/upload/v1700413484/CyberpunkCity_dzadne.jpg' onLoad={handleImageLoad}></img>
+    </div>
   )
 }
 
