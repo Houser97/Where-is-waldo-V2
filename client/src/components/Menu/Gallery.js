94e38ea2a57919a22fc64d42a59b4fcd197dd937
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-22 18:15:36 -0600

    Create ResponsiveGame function and import useWindowSize hooks
    The ResponsiveGame return the links to select the available games, which
    are wrapped in a div to improve the application responsiveness.

--- client/src/components/Menu/Gallery.js
+++ client/src/components/Menu/Gallery.js
@@ -3,19 +3,41 @@ import '../../styles/Menu/Gallery.css'
 import { Link } from "react-router-dom";
 import { useImageIsLoaded } from '../hooks/useImageIsLoaded';
 import Loader from '../Loader';
+import useWindowSize from '../../hooks/windowSizeHook';
 
 const Gallery = () => {
 
   const [isImageLoaded, handleImageLoad] = useImageIsLoaded();
+  const { width, height } = useWindowSize();
+
+  const games = [
+    { to: 'Cyberpunk-City', className: 'gallery one', dataText: 'Cyberpunk City' },
+    { to: 'Robot-City', className: 'gallery two', dataText: 'Robot City' },
+    { to: 'Universe-113', className: 'gallery three', dataText: 'Universe 113' },
+  ];
+
+  const ResponsiveGames = () => {
+    if (width >= 600) {
+      return <>
+        {games.map((game, index) => (
+          <Link key={index} to={game.to} className={game.className} data-text={game.dataText}></Link>
+        ))}
+      </>
+    }
+    return <div>
+
+      {games.map((game, index) => (
+        <Link key={index} to={game.to} className={game.className} data-text={game.dataText}></Link>
+      ))}
+
+    </div>
+  }
 
   return (
-    <div className={`${isImageLoaded ? 'gallery__container' : 'loader__container'} `}>
-      {isImageLoaded
-        ? <>
-          <Link to='Cyberpunk-City' className="gallery one" data-text="Cyberpunk City"></Link>
-          <Link to='Robot-City' className="gallery two" data-text="Robot City"></Link>
-          <Link to='Universe-113' className="gallery three" data-text="Universe 113"></Link>
-        </>
+    <div className={`${isImageLoaded ? 'gallery__container' : 'loader__container'}`}>
+      {isImageLoaded ? (
+        ResponsiveGames()
+      )
         : <Loader />}
       <img className='gallery__image-loader' alt='board' src='https://res.cloudinary.com/dluwqcce9/image/upload/v1700413484/CyberpunkCity_dzadne.jpg' onLoad={handleImageLoad}></img>
     </div>
