1f4e6b890d81e3d99305284c886443fc67d2f273
Arturo Rivera <arturo.riverar97@gmail.com>
2023-12-24 17:45:28 -0600

    Remove Timer stylesheet

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,5 +1,4 @@
 import { useContext, useEffect, useState } from 'react';
-import '../styles/Timer.css';
 import { gameContext } from '../App';
 
 const Timer = () => {
@@ -12,44 +11,44 @@ const Timer = () => {
 
     useEffect(() => {
         let intervalId;
-        if(!gameOver && startGame){
-                intervalId =  setInterval(() => {
+        if (!gameOver && startGame) {
+            intervalId = setInterval(() => {
                 setSeconds(oldSeconds => oldSeconds + 1);
             }, 1000)
-        }else if(gameOver){
+        } else if (gameOver) {
             getTime(seconds);
         }
 
         return () => {
             clearInterval(intervalId);
         };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [gameOver, startGame]);
 
 
-    function time_convert(num){ 
-        const minutes = Math.floor(num / 60);  
+    function time_convert(num) {
+        const minutes = Math.floor(num / 60);
         const seconds = num % 60;
         let minutesToDisplay;
-        if(minutes <= 9){
-            minutesToDisplay = `0${minutes}`;   
+        if (minutes <= 9) {
+            minutesToDisplay = `0${minutes}`;
         } else {
             minutesToDisplay = `${minutes}`
-        } 
+        }
         if (seconds <= 9) {
             return `${minutesToDisplay}:0${seconds}`;
         } else {
             return `${minutesToDisplay}:${seconds}`;
-        }     
+        }
     }
 
-    return(
+    return (
         <div className='timer' id="remove">{
             time_convert(seconds)
         }</div>
     )
-            
-    
+
+
 }
 
 export default Timer;
