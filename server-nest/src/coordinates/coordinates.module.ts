32a615311115f00a8dc7468f7145f17167a5cd17
Arturo Rivera <arturo.riverar97@gmail.com>
2024-10-07 08:02:18 -0600

    Add CoordinatesSchema to CoordinatesModule imports

--- server-nest/src/coordinates/coordinates.module.ts
+++ server-nest/src/coordinates/coordinates.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CoordinatesController } from './coordinates.controller';
 import { CoordinatesService } from './coordinates.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Coordinates, CoordinatesSchema } from './entities/coordinates.entity';
 
 @Module({
   controllers: [CoordinatesController],
-  providers: [CoordinatesService]
+  providers: [CoordinatesService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Coordinates.name,
+        schema: CoordinatesSchema
+      }
+    ])
+  ]
 })
-export class CoordinatesModule {}
+export class CoordinatesModule { }
