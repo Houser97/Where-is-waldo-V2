3fcf8dce20f6bad749551cc9bcb6ddcdca923a9b
Arturo Rivera <arturo.riverar97@gmail.com>
2024-10-15 08:26:48 -0600

    Set payload max value

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(express.json({ limit: '10mb' })); // Adjust the limit as needed
+  app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
   await app.listen(3000);
 }
 bootstrap();
